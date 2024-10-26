1fffbe4fd563fb88172088e030b31c9e885eecff
Wesley Xavier <wesley.xvier@gmail.com>
2024-10-26 17:01:33 -0300

    :lipstick: feat: added script to perform partial evaluation of each module.

---
+++ Frontend/src/evaluations/script.js
@@ -0,0 +1,180 @@
+function ScrumQuestions (){
+    let contador = 0;
+    const quest1 = document.getElementById('a3');
+    const quest2 = document.getElementById('b2');
+    const quest3 = document.getElementById('c2');
+    const quest4 = document.getElementById('d3');
+    const quest5 = document.getElementById('e3');
+    const quest6 = document.getElementById('f2');
+    const quest7 = document.getElementById('g1');
+    const quest8 = document.getElementById('h2');
+    const quest9 = document.getElementById('i2');
+    const quest10 = document.getElementById('j1');
+    if(quest1.checked){
+        contador += 1;
+    }
+    if(quest2.checked){
+        contador += 1;
+    }
+    if(quest3.checked){
+        contador += 1;
+    }
+    if(quest4.checked){
+        contador += 1;
+    }
+    if(quest5.checked){
+        contador += 1;
+    }
+    if(quest6.checked){
+        contador += 1;
+    }
+    if(quest7.checked){
+        contador += 1;
+    }
+    if(quest8.checked){
+        contador += 1;
+    }
+    if(quest9.checked){
+        contador += 1;
+    }
+    if(quest10.checked){
+        contador += 1;
+    }
+    document.getElementById('result-panel').hidden = false;
+    document.getElementById('result').innerHTML = ' ' + contador + ' de 10';
+}
+function PoQuestions (){
+    let contador = 0;
+    const quest1 = document.getElementById('a2');
+    const quest2 = document.getElementById('b2');
+    const quest3 = document.getElementById('c3');
+    const quest4 = document.getElementById('d1');
+    const quest5 = document.getElementById('e3');
+    const quest6 = document.getElementById('f1');
+    const quest7 = document.getElementById('g3');
+    const quest8 = document.getElementById('h2');
+    const quest9 = document.getElementById('i4');
+    const quest10 = document.getElementById('j2');
+    if(quest1.checked){
+        contador += 1;
+    }
+    if(quest2.checked){
+        contador += 1;
+    }
+    if(quest3.checked){
+        contador += 1;
+    }
+    if(quest4.checked){
+        contador += 1;
+    }
+    if(quest5.checked){
+        contador += 1;
+    }
+    if(quest6.checked){
+        contador += 1;
+    }
+    if(quest7.checked){
+        contador += 1;
+    }
+    if(quest8.checked){
+        contador += 1;
+    }
+    if(quest9.checked){
+        contador += 1;
+    }
+    if(quest10.checked){
+        contador += 1;
+    }
+    document.getElementById('result-panel').hidden = false;
+    document.getElementById('result').innerHTML = ' ' + contador + ' de 10';
+}
+function ScrumMasterQuestions (){
+    let contador = 0;
+    const quest1 = document.getElementById('a2');
+    const quest2 = document.getElementById('b2');
+    const quest3 = document.getElementById('c2');
+    const quest4 = document.getElementById('d2');
+    const quest5 = document.getElementById('e3');
+    const quest6 = document.getElementById('f3');
+    const quest7 = document.getElementById('g3');
+    const quest8 = document.getElementById('h3');
+    const quest9 = document.getElementById('i3');
+    const quest10 = document.getElementById('j3');
+    if(quest1.checked){
+        contador += 1;
+    }
+    if(quest2.checked){
+        contador += 1;
+    }
+    if(quest3.checked){
+        contador += 1;
+    }
+    if(quest4.checked){
+        contador += 1;
+    }
+    if(quest5.checked){
+        contador += 1;
+    }
+    if(quest6.checked){
+        contador += 1;
+    }
+    if(quest7.checked){
+        contador += 1;
+    }
+    if(quest8.checked){
+        contador += 1;
+    }
+    if(quest9.checked){
+        contador += 1;
+    }
+    if(quest10.checked){
+        contador += 1;
+    }
+    document.getElementById('result-panel').hidden = false;
+    document.getElementById('result').innerHTML = ' ' + contador + ' de 10';
+}
+function DevTeamQuestions (){
+    let contador = 0;
+    const quest1 = document.getElementById('a3');
+    const quest2 = document.getElementById('b3');
+    const quest3 = document.getElementById('c3');
+    const quest4 = document.getElementById('d3');
+    const quest5 = document.getElementById('e3');
+    const quest6 = document.getElementById('f3');
+    const quest7 = document.getElementById('g3');
+    const quest8 = document.getElementById('h3');
+    const quest9 = document.getElementById('i3');
+    const quest10 = document.getElementById('j3');
+    if(quest1.checked){
+        contador += 1;
+    }
+    if(quest2.checked){
+        contador += 1;
+    }
+    if(quest3.checked){
+        contador += 1;
+    }
+    if(quest4.checked){
+        contador += 1;
+    }
+    if(quest5.checked){
+        contador += 1;
+    }
+    if(quest6.checked){
+        contador += 1;
+    }
+    if(quest7.checked){
+        contador += 1;
+    }
+    if(quest8.checked){
+        contador += 1;
+    }
+    if(quest9.checked){
+        contador += 1;
+    }
+    if(quest10.checked){
+        contador += 1;
+    }
+    document.getElementById('result-panel').hidden = false;
+    document.getElementById('result').innerHTML = ' ' + contador + ' de 10';
+}

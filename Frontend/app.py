eca5ad1fff93f10e2b06cbb7c99b7fcaa2dd28a7
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-14 13:37:05 -0300

    :bug: fix: resolve main links

--- Frontend/app.py
+++ Frontend/app.py
@@ -6,3 +6,45 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/modules')
+def modules():
+    return render_template('Modulos.html')
+
+@app.route('/DevTeamTail')
+def devTeamTail():
+    return render_template('DevTeamTail.html')
+
+@app.route('/ProductOwner')
+def ProductOwner():
+    return render_template('ProductOwner.html')
+
+@app.route('/ScrumTail')
+def ScrumTail():
+    return render_template('ScrumTail.html')
+
+@app.route('/ScrumMaster')
+def ScrumMaster():
+    return render_template('ScrumTail.html')
+
+# Submodulos routes
+
+@app.route('/DevTeamQuestions')
+def DevTeam():
+    return render_template('/evaluations/DevTeamQuestions.html')
+
+@app.route('/ProductOwnerQuestions')
+def Po():
+    return render_template('/evaluations/PoQuestions.html')
+
+@app.route('/ScrumMasterQuestions')
+def ScrumM():
+    return render_template('/evaluations/ScrumMasterQuestions.html')
+
+@app.route('/ScrumQuestions')
+def Scrum():
+    return render_template('/evaluations/ScrumQuestions.html')
+
+@app.route('/FinalExamQuestions')
+def FinalExam():
+    return render_template('/evaluations/FinalExam.html')

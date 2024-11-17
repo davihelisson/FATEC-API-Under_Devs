ee97d86a240f08208d526c91a0b931553cd8acb2
João Álvaro <91163460+JoaoAlv4ro@users.noreply.github.com>
2024-11-17 20:12:30 +0000

    :iphone: feat: responsividade home

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./templates/**/*.html",
+    "./static/src/**/*.js"
   ],
   theme: {
     extend: {
